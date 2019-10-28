ab339609c5e68fd0a61e6c853cdfb5a9e092347f
Alex Bokov <bokov@uthscsa.edu>
2019-10-28 12:06:25 -0500

    config.R; config; Updated leading paragraph, since now 'config.R' is the project-level config, and the user/machine-level config will now be 'config.local.R'. Updated some of the other comments, and added a new config variable: 'na.strings'.

--- config.R
+++ config.R
@@ -3,20 +3,30 @@
 #' author: "Alex F. Bokov, Ph.D."
 #' date: "10/18/2018"
 #' ---
+#' This is just an example of what computer-specific variables should be set. 
+#' Modify this file according to the needs of your project. For variables that
+#' will vary from one computer to another, instruct your 
+#' contributors/collaborators to create their own file called `config.local.R`
+#' in the same directory as this one, and _not_ check that file into the repo.
 #' 
-#' Please copy this file to `config.R`, edit that copy, and copy it over to
-#' the working directory whenever you check out this project. This is just an
-#' example of what computer-specific variables should be set. A file needs to be 
-#' called `config.R` in order to be used as a source of configuration 
-#' information by our scripts
-#' 
+#' This points to an example dataset for testing purposes. The real dataset 
+#' should reside on individual collaborators' machines and the `inputdata`
+#' variable set in their individual `config.local.R` will be used in preference 
+#' to the path below.
 inputdata <- 'data/example.csv';
+
+#' For reproducible yet approximately random number generation (e.g. sampling)
 project_seed <- 20191022;
-#' 
+
+#' If you want to override which strings will be treated as missing data, change
+#' the variable below.
+na.strings <- c('NA','','-','(null)');
 #' Do you know what character is used to delimit your files? Usually it's either
 #' comma or tab, but it could be anything. If you're not sure, leave this 
 #' commented out, and the scripts will try to guess it for you.
+#' (this setting not yet fully implemented)
 #file_delim <- '\t';
+
 #' Your data dictionary file (if/when you have one, uncomment this line)
 #dctfile_raw <- 'WHERE_I_KEEP_MY_DATA/MY_DATA_DICTIONARY.csv';
 
