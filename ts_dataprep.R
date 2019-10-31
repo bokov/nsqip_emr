3e0c46aee53ae0724aa64ab183eb0d6233cbec10
Alex Bokov <bokov@uthscsa.edu>
2019-10-31 17:06:26 -0500

    ts_dataprep.R; core; Refactored data transformation to be faster and FINALLY to have a correct sorting variable to use as the y-axis thanks to 'fread()', the 'data.table' equivalent of 'dense_rank()' in 'dplyr'. And some hacky conversion to and from factors for the sampled data.

--- ts_dataprep.R
+++ ts_dataprep.R
@@ -42,47 +42,121 @@ if(.debug>1) source('./scripts/global.R',chdir=T) else {
 #===========================================================#
 
 #' ### Preparing the Data
-#' Get some aggregate columns for grouping similar timelines
-dat01rank <- group_by(dat00,CASE_DEID) %>% 
-  summarize(min=min(c(TIME_TO_EVENT,0),na.rm=TRUE)
-            ,max=max(c(TIME_TO_EVENT,0),na.rm = TRUE)
-            ,rng=max-min
-            ,nn=n()
-            ,adm=min(TIME_TO_EVENT[EVENT=='AdmitDt'])
-            ,dsc=max(TIME_TO_EVENT[EVENT=='DischargeDt'&SOURCE=='NSQIP'])
-            ,maxpre = max(TIME_TO_EVENT[TIME_TO_EVENT<0 & 
-                                      EVENT=='PreAdmit1|Discharge:INPATIENT'])
-            ,minpost = min(TIME_TO_EVENT[TIME_TO_EVENT>dsc & 
-                                           EVENT=='PostAdmit1|Admit:INPATIENT'])
-            ) %>% 
-  # rmin amounts to a unique integer id for each case, helpful for tie breaking
-  arrange(rng) %>% mutate(rmin=seq_len(n())) %>% as.data.table %>% 
-  setkey(CASE_DEID);
+#' Get some aggregate columns for grouping similar timelines. All this chunk of
+#' code below is not separate statements horribly formatted. It's technically 
+#' one line of code, `data.table`'s equivalent of a pipeline. I'm not doing this
+#' because I want to. If I did this with `dplyr` or base, it would take much 
+#' longer than the `data.table` version.
+#+ dat01rank
+# dat01rank ----
 
-#' Merge the aggregate columns on the full dataset
-# dat01 ----
-dat01 <- as.data.table(dat00) %>% setkey(CASE_DEID) %>% `[`(dat01rank) %>%
-  # create a combined source-event variable
-  {(.)[,`:=`(src_evt=paste0(SOURCE,'|',EVENT)
-             # group various source-event combos into types
-             ,evt_type=recode(EVENT,AdmitDt='Admit',DischargeDt='Discharge'
-                              ,DischargeOrder='Discharge',SurgStartTime='Surg'
-                              ,SurgEndTime='Surg',.default='Misc Orders')
-             # variable by which to order the results for ggplot, first by
-             # non-NSQIP admission date (if different from NSQIP's)
-             ,order00=rank(rank(adm)*1e8 +
-                             # then by earliest event preceding NSQIP admission
-                             # date, if there are any such events
-                             rank(min)*1e4 +
-                             # then by NSQIP discharge date
-                             rank(dsc) +
-                             # then by the last recorded event of any type
-                             rank(max)*1e-4))]}
+dat01 <- as.data.table(dat00, key = 'CASE_DEID')[, `:=`(
+  min = min(c(TIME_TO_EVENT, 0), na.rm = TRUE)
+  ,max = max(c(TIME_TO_EVENT, 0), na.rm = TRUE)
+  ,nn = .N ,adm = min(TIME_TO_EVENT[EVENT == 'AdmitDt'], na.rm = TRUE)
+  ,dsc = max(TIME_TO_EVENT[EVENT == 'DischargeDt' &
+                             SOURCE == 'NSQIP'], na.rm = TRUE)
+  ,maxpre = max(TIME_TO_EVENT[TIME_TO_EVENT < 0 &
+                                EVENT =='PreAdmit1|Discharge:INPATIENT']
+                , na.rm =TRUE))
+  , by = CASE_DEID][, `:=`(
+    minpost = min(TIME_TO_EVENT[TIME_TO_EVENT > dsc &
+                                  EVENT =='PostAdmit1|Admit:INPATIENT']
+                  , na.rm =TRUE)
+    ,rng = max - min), by = CASE_DEID][
+      , rngprepost := diff(range(pmax(min, maxpre), pmin(max, minpost)))
+      ,by =CASE_DEID][
+        , `:=`(
+          chk_readmovrlp = min(
+            TIME_TO_EVENT[EVENT == 'PostAdmit1|Admit:INPATIENT']) < dsc
+          ,chk_redscovrlp = min(
+            TIME_TO_EVENT[EVENT == 'PostAdmit1|Discharge:INPATIENT']) <
+            min(TIME_TO_EVENT[EVENT =='PostAdmit1|Admit:INPATIENT'])
+          ,chk_dischovrlp = max(
+            TIME_TO_EVENT[EVENT == 'PreAdmit1|Discharge:INPATIENT']) > 0
+          ,chk_admitovrlp = max(
+            TIME_TO_EVENT[EVENT == 'PreAdmit1|Admit:INPATIENT']) >
+            max(TIME_TO_EVENT[EVENT =='PreAdmit1|Discharge:INPATIENT'])
+          ,chk_missingdsc = is.infinite(dsc))
+        ,by = CASE_DEID][, `:=`(
+            src_evt = paste0(SOURCE, '|', EVENT)
+            # group various source-event combos into types
+            ,evt_type = recode(EVENT,AdmitDt = 'Admit'
+                               ,DischargeDt = 'Discharge'
+                               ,DischargeOrder = 'Discharge'
+                               ,SurgStartTime = 'Surg',SurgEndTime = 'Surg'
+                               ,.default = 'Misc Orders'))][
+                                 , chk_total := chk_readmovrlp + 
+                                     chk_redscovrlp + chk_dischovrlp + 
+                                     chk_admitovrlp + chk_missingdsc
+                                 ,by = CASE_DEID];
+dat01$order00 <- frank(dat01,adm,min,dsc,max,rng,CASE_DEID,ties.method='dense');
+dat01$order01 <- frank(dat01,adm,maxpre,dsc,minpost,rngprepost,CASE_DEID
+                       ,ties.method='dense');
+# [,order00 := frank(adm, min, dsc, max, rng, ties.method = 'random')
+#        ,by = list(CASE_DEID)];
+# 
+#,order01 = frank(., adm, maxpre, dsc, minpost, rngprepost
+#                 , ties.method ='random'))
+#' 
+#' #' Merge the aggregate columns on the full dataset
+#' #+ dat01
+#' # dat01 ----
+#' dat01 <- as.data.table(dat00) %>% setkey(CASE_DEID) %>% `[`(dat01rank) %>%
+#'   # data.table modifies values by reference, so have to pipe through copy in 
+#'   # order to avoid messing up the original
+#'   copy %>% 
+#'   # create a combined source-event variable
+#'   {(.)[,`:=`(src_evt=paste0(SOURCE,'|',EVENT)
+#'              # group various source-event combos into types
+#'              ,evt_type=recode(EVENT,AdmitDt='Admit',DischargeDt='Discharge'
+#'                               ,DischargeOrder='Discharge',SurgStartTime='Surg'
+#'                               ,SurgEndTime='Surg',.default='Misc Orders')
+#'              # variable by which to order the results for ggplot, first by
+#'              # non-NSQIP admission date (if different from NSQIP's)
+#'              )]};
+#' #' Validity tests for dat00 and dat01
+stopifnot(identical(names(dat00)
+                    ,c('CASE_DEID','TIME_TO_EVENT','SOURCE','EVENT')));
+stopifnot(with(dat01,length(unique(CASE_DEID))==length(unique(order00))));
+stopifnot(with(dat01,length(unique(CASE_DEID))==length(unique(order01))));
 
-# take a random sample 
-dat01a <- subset(dat01,CASE_DEID %in% sample(dat01$CASE_DEID
-                                        ,getOption('project.sample',1000))) %>%
-  {(.)[,order01 := rank(order00)]};
+#' Keep only the events surrounding the NSQIP index stay that happen after the
+#' previous discharge and before the next readmit
+#+ dat02
+# dat02 ----
+dat02 <- dat01[TIME_TO_EVENT >= maxpre & TIME_TO_EVENT <= minpost,][
+  ,order00:=order01];
+stopifnot(with(dat02,length(unique(CASE_DEID))==length(unique(order00))));
+stopifnot(with(dat02,all(order00==order01)));
+
+
+#' Take a random sample 
+#+ dat01a
+# dat01a ----
+dat01a <- dat01[CASE_DEID %in% sample(dat01$CASE_DEID
+                                      ,getOption('project.sample',1000))
+                ,][# collapse the gaps in the original ranking. Yes, clumsy,
+                   # didn't say I was proud of this...
+                   ,`:=`(order00 = as.numeric(ordered(order00))
+                         ,order01 = as.numeric(ordered(order01)))];
+stopifnot(with(dat01a
+               ,length(union(seq_along(unique(CASE_DEID))
+                             ,unique(order00)))==length(unique(CASE_DEID))));
+stopifnot(with(dat01a
+               ,length(union(seq_along(unique(CASE_DEID))
+                             ,unique(order01)))==length(unique(CASE_DEID))));
+
+# Keep only the events surrounding the NSQIP index stay that happen after the
+# previous discharge and before the next readmit, for the random sample
+#+ dat02a
+# dat02a ----
+dat02a <- dat01a[TIME_TO_EVENT >= maxpre & TIME_TO_EVENT <= minpost,][
+  ,order00:=order01];
+stopifnot(with(dat02a
+               ,length(union(seq_along(unique(CASE_DEID))
+                             ,unique(order00)))==length(unique(CASE_DEID))));
+stopifnot(with(dat02a,all(order00==order01)));
 
 if(file.exists('eventmap.csv')){
   dct0 <- try_import('eventmap.csv');
