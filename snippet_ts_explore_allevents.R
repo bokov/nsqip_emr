ec1e826ec2a8fb8511c7fd8d74afdec1d5509989
Alex Bokov <bokov@uthscsa.edu>
2020-01-05 22:02:52 -0600

    snippet_ts_explore_allevents.R; feature; Now 'geom_line()' color is also dynamic, also outputs a ggplot object named '.toplot' for debugging purposes

--- snippet_ts_explore_allevents.R
+++ snippet_ts_explore_allevents.R
@@ -7,10 +7,11 @@
 #' `source('snippet_ts_explore_allevents.R',local=T)`
 #' 
 #+ main, eval=FALSE, echo=TRUE
-.input %>% 
+.toplot <- .input %>% 
 {ggplot(.,aes(x=TIME_TO_EVENT,y=order.active,group=CASE_DEID,shape=src_evt
               ,color=src_evt,size=src_evt)) + 
-    geom_line(alpha=0.1,color='black',size=0.5) + 
+    #geom_line(alpha=0.1,color='black',size=0.5) + 
+    geom_line(aes(color=CHK_lncolor),alpha=0.05,size=0.4,show.legend = FALSE) +
     geom_point(data=subset((.),src_evt %in% v(c_misc)&
                              TIME_TO_EVENT < .xlim[2] &
                              TIME_TO_EVENT > .xlim[1])
@@ -31,17 +32,24 @@
                              TIME_TO_EVENT > .xlim[1] &
                              TIME_TO_EVENT < .xlim[2])
                ) + 
-    scale_shape_manual(limits=dct0$src_evt,values = dct0$shape) +
-    scale_color_manual(limits=dct0$src_evt,values = dct0$color) +
-    scale_size_manual(limits=dct0$src_evt,values= dct0$size) +
+    #colscl +
+    scale_color_manual(limits=dct0$label,values = dct0$color) +
+    scale_shape_manual(limits=dct0$label,values = dct0$shape) +
+    scale_size_manual(limits=dct0$label,values= dct0$size) +
     scale_x_continuous(limits = .xlim,oob=squish) +
     xlab('Days from NSQIP Admission Date') +
     guides(color=guide_legend('Source|Event',ncol=2)
            ,shape=guide_legend('Source|Event',ncol=2)
            ,size=guide_legend('Source|Event',ncol=2)
            ) +
+    # new_scale_color() +
+    # scale_color_manual(limits=c(names(chk_colors),'multi','none'),values = c(chk_colors
+    #                               ,multi='#FF0000'
+    #                               ,none='#000000')) +
+    #guides(color=guide_legend('Discrepancy Type')) +
     theme(axis.text.y=element_blank(),axis.title.y=element_blank()
           ,axis.ticks.y=element_blank()
           ,text=element_text(family="Times New Roman")
           ,legend.position = 'bottom')
-  } %>% print
+  }
+print (.toplot)
